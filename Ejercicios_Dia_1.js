313b357bedcacdcb343031817825c57268cc6230
Daniel Romero <dsromeror@unal.edu.co>
2021-04-11 19:08:03 -0500

    [Update] Se eliminana enunciados en los ejercicios resueltos

--- Ejercicios_Dia_1.js
+++ Ejercicios_Dia_1.js
@@ -2,55 +2,31 @@
 
 const ps = require("prompt-sync");
 const prompt = ps();
-/*Ejercicio 1
-Escribe un programa que le pida al usuario ingresar una frase y la imprima en la consola.
-Recuerda que para pedirle al usuario que escriba una frase debes utilizar el método prompt() 
-y para escribir en la consola debes utilizar el método console.log().
-Comparte tu respuesta en este enlace.*/
+
+/*Ejercicio 1*/
 
 let mensaje = prompt("Ingresa un mensaje: ");
 console.log(mensaje);
 
-/*Ejercicio 2
-
-Escribe un programa que le pregunte al usuario su nombre e imprima "Hola " seguido del nombre 
-y un signo de exclamación. Por ejemplo, si el usuario ingresa "Pedro" el programa debe imprimir en 
-la consola "Hola Pedro!". Comparte tu respuesta en este enlace*/
+/*Ejercicio 2*/
 
 let nombre = prompt("¿Cual es tu nombre?");
 console.log("Hola " + nombre + "!");
 
-/*Ejercicio 3
-
-Escribe un programa que le pida al usuario ingresar un número, luego le pide un segundo número, 
-e imprima en la consola la suma de los dos números que ingresó el usuario. Comparte tu respuesta en este enlace*/
+/*Ejercicio 3*/
 
 let numero1 = parseInt(prompt("Ingrese un número: "));
 let numero2 = parseInt(prompt("Ingrese otro número: "));
 
 console.log ("La suma de los números es: " +  (numero1 + numero2));
 
-/*Ejercicio 4
-
-Escribe un programa que le pida al usuario su año de nacimiento e imprima su edad actual en la consola con 
-la frase "Tienes X años". Por ejemplo, asumiendo que el año actual es 2020 y el usuario ingresa 2000, el programa 
-debe imprimir en la consola:
-Tienes 20 años
-
-Comparte tu respuesta en este enlace*/
+/*Ejercicio 4*/
 
 let nacimiento = parseInt(prompt("Digite su año de nacimiento: "));
 let edad = 2021 - nacimiento;
 console.log("Tienes " + edad + " años");
 
-/*Ejercicio 5
-
-El índice de masa corporal (IMC), o BMI por sus siglas en inglés, es un valor que determina la cantidad de grasa de una persona.
-El BMI se calcula con la siguiente formula:
-peso / altura^2
-Escribe un programa que le pida al usuario su peso y su altura para calcular su BMI e imprima la frase "Tu BMI es X".
-Por ejemplo, si el usuario ingresa 65 de peso y 1.8 de altura el programa debe imprimir en la consola la frase "Tu BMI es 20.061728395061728".
-Comparte tu respuesta en este enlace*/
+/*Ejercicio 5*/
 
 let peso = parseFloat(prompt("Ingrese su peso en Kg: "));
 let estatura = parseFloat(prompt("Ingrese su estatura en metros: "));
--- Ejercicios_Dia_2.js
+++ Ejercicios_Dia_2.js
@@ -2,47 +2,28 @@
 
 const ps = require("prompt-sync");
 const prompt = ps();
-/*Ejercicio 6
 
-Escribe un programa que le pida un número al usuario e imprima en la consola si el número es mayor o menor/igual a 10.
-Si es mayor debe imprimir "El número es mayor a 10".
-Si es menor debe imprimir "El número es menor o igual a 10".
-Comparte tu respuesta en este enlace.*/
+/*Ejercicio 6.*/
+
 let numero = parseInt(prompt("Ingrese un número: "));
 if (numero>10){
     console.log("El número es mayor a 10");
 }else{
     console.log("El número es menor o igual a 10")
 }
-/*Ejercicio 7
 
-Escribe un programa que piense un número de forma aleatoria del 1 al 10 y le pida al usuario que lo trate de adivinar. 
-Si el número es correcto debe imprimir en la consola "Felicitaciones, ese era!", de lo contrario debe imprimir 
-"Lo siento, intenta nuevamente!"
+/*Ejercicio 7*/
 
-Comparte tu respuesta en este enlace.*/
 let numeroAleatorio = Math.floor(Math.random()* 10 + 1);
 let numeroUsuario = parseInt(prompt("Ingrese un número: "));
 numeroUsuario == numeroAleatorio ? console.log("Felicitaciones, ese era!") : console.log("Lo siento, intenta nuevamente!");
 
-/*Ejercicio 8
+/*Ejercicio 8*/
 
-Un múltiplo de 5 es aquel que dividido por 5 da por resultado un número entero (sin residuo). Por ejemplo 10, 15, 20, etc. 
-son múltiplos de 5. 11 no es múltiplo de 5 porque quedaría un residuo de 1.
-Escribe un programa que le pida al usuario un número e imprima si es un múltiplo de 5 o no.
-Si es múltiplo debe imprimir "Si, el número x es múltiplo 5", de lo contrario debe imprimir "No, el número x no es múltiplo de 5".
-
-Comparte tu respuesta en este enlace.*/
 let numeroEjercicioOcho = parseInt(prompt("Ingrese un número: "));
 numeroEjercicioOcho%5 == 0 ? console.log("Si, el número x es múltiplo 5") : console.log("No, el número x no es múltiplo de 5");
 
-/*Ejercicio 9
-
-Escribe un programa que le pida un número al usuario e imprima en la consola si el número si es mayor, menor o igual que 10.
-Si el número es menor a 10 debe imprimir "El número es menor que 10".
-Si el número es mayor a 10 debe imprimir "El número es mayor que 10".
-Si el número es igual a 10 debe imprimir "El número es igual a 10".
-Comparte tu respuesta en este enlace.*/
+/*Ejercicio 9*/
 
 let numeroEjercicioNueve = parseInt(prompt("Ingrese un número: "));
 if (numeroEjercicioNueve<10){
@@ -53,19 +34,7 @@ if (numeroEjercicioNueve<10){
     console.log("El número es igual a 10");
 }
 
-/*Ejercicio 10
-
-El índice de masa corporal (IMC), o BMI por sus siglas en inglés, es un valor que determina la cantidad de grasa de una persona.
-El BMI se calcula con la siguiente formula:
-peso / altura^2
-Escribe un programa que le pida al usuario su peso y altura. El programa deberá calcular el BMI e imprimir:
-
-    "Bajo de peso" si el BMI < 18.5
-    "Normal" si está entre 18.5 y 24.9
-    "Sobrepeso" si está entre 25 y 29.9
-    "Obeso" si es igual o mayor a 30
-
-Comparte tu respuesta en este enlace.*/
+/*Ejercicio 10*/
 
 let peso = parseFloat(prompt("Ingrese su peso en Kg: "));
 let estatura = parseFloat(prompt("Ingrese su estatura en metros: "));
@@ -81,16 +50,7 @@ if (bmi<18.5){
     console.log("Obeso");
 }
 
-/*Ejercicio 11
-
-Escribe un programa que le pida al usuario ingresar un número.
-
-    Si el número es múltiplo de 3 debe imprimir en la consola "bing".
-    Si el número es múltiplo de 5 debe imprimir en la consola "bong".
-    Si el número es múltiplo tanto de 3 como de 5 debe imprimir en la consola "bingbong".
-    Si no cumple ninguna de las condiciones anteriores debe imprimir el mismo número.
-
-Comparte tu respuesta en este enlace.*/
+/*Ejercicio 11*/
 
 let numeroEjercicioOnce = parseInt(prompt("Ingresa un número: "));
 if (numeroEjercicioOnce%3 == 0 && numeroEjercicioOnce%5 ==0){
